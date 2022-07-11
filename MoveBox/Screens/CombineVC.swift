5122835b471616ae54a2dd20d7f6ce2b243f0080
Anatolii Ostapenko <anatoliy.ostapenko@gmail.com>
2022-07-11 12:13:52 +0300

    import Combine framework

--- MoveBox/Screens/CombineVC.swift
+++ MoveBox/Screens/CombineVC.swift
@@ -6,18 +6,48 @@
 //
 
 import UIKit
+import Combine
+
+extension Notification.Name {
+    static let newBlogPost = Notification.Name("newPost")
+}
+
+struct BlogPost { let title: String }
 
 class CombineVC: UIViewController {
     
     var blogTextField = CustomTextField(placeholder: "New Blog Post")
     let publishButton = ActionButton(text: "Publish")
-    let subscribeLabel = CustomLabel(text: "Subscriber")
+    var subscribeLabel = CustomLabel(text: "Subsrcibe")
     private let topStackView = CustomStackView(frame: .zero)
     private let bottomStackView = CustomStackView(frame: .zero)
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         configure()
+        setPublishButton()
+        subscriberHandling()
+    }
+    
+    func subscriberHandling() {
+        // Create Publisher
+        let publisher = NotificationCenter.Publisher(center: .default, name: .newBlogPost, object: nil)
+            .map { (notification) -> String? in // Combine with operator
+                return (notification.object as? BlogPost)?.title ?? ""
+            }
+        // Create Subscriber
+        let subscriber = Subscribers.Assign(object: subscribeLabel, keyPath: \.text)
+        publisher.subscribe(subscriber)
+    }
+    
+    func setPublishButton() {
+        publishButton.addTarget(self, action: #selector(publishButtonPressed(_:)), for: .primaryActionTriggered)
+    }
+    
+    @objc func publishButtonPressed(_ sender: UIButton) {
+        let title = blogTextField.text ?? "Comming soon"
+        let blogPost = BlogPost(title: title)
+        NotificationCenter.default.post(name: .newBlogPost, object: blogPost)
     }
     
     private func configure() {
